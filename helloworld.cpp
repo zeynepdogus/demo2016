0a998d6e689eff239bc685cf0a049ae9016d2f4c
zeynep <doguszeynep@gmail.com>
2015-12-09 15:18:06 +0200

    f

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,33 +1,10 @@
-/**
-* @file   hello.cpp
-* @author Andriy Petlovanyy <software@shadowrobot.com>
-*
-* Copyright 2015 Shadow Robot Company Ltd.
-*
-* This program is free software: you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the Free
-* Software Foundation, either version 2 of the License, or (at your option)
-* any later version.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*
-* @brief Sample C++ Library.
-*
-*/
+// A hello world program in C++
 
-#include <iostream>
-#include <string>
+	#include<iostream>
+	using namespace std;
 
-int main()
-{
-  std::cout << "Hello World!" << std::endl;
-  return 0;
-}
-
+	int main()
+	{
+		cout << "Hello World!";
+		return 0;
+	}
